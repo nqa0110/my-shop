233050c804986ca8b3e2b7368bc377b4846a3786
nqa0110 <nguyenandie1905@gmail.com>
2021-04-05 23:58:19 +0700

    add

--- Main/FE/my-shop/src/app/app.module.ts
+++ Main/FE/my-shop/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // form
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; // call api, INTERCEPTORS
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard'; // check login
+import { TokenInterceptorService } from './services/token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // two-way binding
+    ReactiveFormsModule, // form
+    HttpClientModule, // call api
   ],
-  providers: [],
+  providers: [
+    AuthService, AuthGuard,{
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
